0985315106e6ffa8ed4e218dba2666066d2aee72
TeddyTodor <theodelacroix@hotmail.com>
2024-10-04 17:51:53 +0200

    Add files via upload

---
+++ Cours 1.py
@@ -0,0 +1,17 @@
+in_value = 3
+name = "Theo"
+print(f"result : ")
+print(f"\t-{in_value}\n"*3)
+
+print("Hello "+ name + " friend," + " \n" + "bye")
+print(f"Hello {name} friend, \nbye")
+
+print(f"""result :
+        -value1 : 1
+        -value2 : 2
+        -value3 : 3 
+        """)
+
+alt = input("Donnez un truc : ")
+res = int(alt)*5+3
+print(res)
