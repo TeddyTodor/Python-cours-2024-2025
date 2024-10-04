55727e4b557ac68ca27f07c352d4019de117fb0a
TeddyTodor <theodelacroix@hotmail.com>
2024-10-04 19:08:07 +0200

    Add files via upload

---
+++ Cours 2/main.py
@@ -0,0 +1,19 @@
+def exo_1():
+
+    firstname = "Jojo"
+    lastname = "Jotaro"
+    age = 17
+    adresse = "Montgomery"
+    sport = "muscu"
+
+    sentence = (f"""
+    Bonjour,
+    Je m'appelle {firstname} {lastname}       
+    J'ai {age} ans
+    J'habite a tatata proche de {adresse}
+    J'aime bien faire de la {sport}
+    Et c'est tout.""")
+
+    print(sentence)
+
+exo_1()
