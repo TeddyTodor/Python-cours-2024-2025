2741c983f75c31062d222706c60bd26a1e4b8b4d
TeddyTodor <theodelacroix@hotmail.com>
2024-11-13 17:33:24 +0100

    Add files via upload

---
+++ Cours 5/main.py
@@ -0,0 +1,36 @@
+number = 1
+#while True :
+    #print(number)
+    #number += 1
+
+number_2 = 1
+sum = 0
+while number_2 < 11 :
+    print(number_2)
+    sum += number_2
+    number_2 += 1
+print(sum)
+
+num = 78456
+count = 0
+while num !=  0:
+    num //= 10
+    count += 1
+print(f"Nombre de chiffre : {count}")
+
+num = 42
+guessnum = int(input("How many guesses should you have ?\n"))
+guess = 0
+while guessnum > 0 and guess != num :
+    guess = int(input("Take a guess \n"))
+    if guess < num :
+        print ("Bigger")
+    elif guess > num :
+        print ("Smaller")
+    guessnum -= 1
+    print(f"You have {guessnum} chances left.")
+if guess == num :
+    print("You win")
+else :
+    print("You lose")
+    
